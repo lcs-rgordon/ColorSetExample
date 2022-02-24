b26cb1b3fd0538789af04afb85033a402cee91a3
Russell Gordon <github@russellgordon.ca>
2022-02-24 16:58:19 -0500

    Initial Commit

---
+++ ColorSetExample/ColorSetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSetExampleApp.swift
+//  ColorSetExample
+//
+//  Created by Russell Gordon on 2022-02-24.
+//
+
+import SwiftUI
+
+@main
+struct ColorSetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
